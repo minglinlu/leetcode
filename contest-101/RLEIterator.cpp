99c99b92beeaffde1f561ef0f139ec5612eedd82
minglinlu <373128042@qq.com>
2019-03-28 18:20:28 +0800

    add contest-101

---
+++ contest-101/RLEIterator.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+  > File Name: loud-and-rich.cpp
+  > Author: luminglin
+  > Mail: luminglin@foxmail.com 
+  > Created Time: Sun Mar  3 18:05:54 2019
+ ************************************************************************/
+
+#include<iostream>
+#include<string>
+#include<stack>
+#include<map>
+#include<set>
+#include<queue>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class RLEIterator {
+public:
+    RLEIterator(vector<int> A) {
+
+    }
+
+    int next(int n) {
+
+		return 0;
+    }
+};
+
+int main() 
+{
+	printf("\n");
+	return 0;
+}
